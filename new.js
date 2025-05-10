49f77b3c20460ba75d836e5d5dbfa0c6ddd1e9ad
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-05-10 20:43:43 +0700

    Update new.js

--- new.js
+++ new.js
@@ -33,7 +33,7 @@ const globalConfig = {
   },
   stakeContracts: {
     ausd:  '0x054de909723ECda2d119E31583D40a52a332f85c',
-    usde:  '0x07f8ec2B79B7A1998Fd0B21a4668B0Cf1cA72C02',
+    usde:  '0x3988053b7c748023a1ae19a8ed4c1bf217932bdb',
     lvlusd:'0x5De3fBd40D4c3892914c3b67b5B529D776A1483A',
     vusd:  '0x5bb9Fa02a3DCCDB4E9099b48e8Ba5841D2e59d51',
     vnusd: '0x2608A88219BFB34519f635Dd9Ca2Ae971539ca60'
