1252287de982350d498ba5df1cbc923cd65bc3ce
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-05-16 13:52:23 +0700

    Update new.js

--- new.js
+++ new.js
@@ -24,24 +24,28 @@ const globalConfig = {
     usde:    '0xf4BE938070f59764C85fAcE374F92A4670ff3877',
     lvlusd:  '0x8802b7bcF8EedCc9E1bA6C20E139bEe89dd98E83',
     vusd:    '0xc14A8E2Fc341A97a57524000bF0F7F1bA4de4802',
-    vnusd:   '0xBEbF4E25652e7F23CCdCCcaaCB32004501c4BfF8'
+    vnusd:   '0xBEbF4E25652e7F23CCdCCcaaCB32004501c4BfF8',
+    AI16Z:   '0x2d5a4f5634041f50180A25F26b2A8364452E3152'
   },
   routers: {
     virtual: '0x3dCACa90A714498624067948C092Dd0373f08265',
     ath:     '0x2cFDeE1d5f04dD235AEA47E1aD2fB66e3A61C13e',
-    vnusd:   '0xEfbAE3A68b17a61f21C7809Edfa8Aa3CA7B2546f'
+    vnusd:   '0xEfbAE3A68b17a61f21C7809Edfa8Aa3CA7B2546f',
+    azUSD :  '0xb0b53d8b4ef06f9bbe5db624113c6a5d35bb7522'
   },
   stakeContracts: {
     ausd:  '0x054de909723ECda2d119E31583D40a52a332f85c',
     usde:  '0x3988053b7c748023a1ae19a8ed4c1bf217932bdb',
     lvlusd:'0x5De3fBd40D4c3892914c3b67b5B529D776A1483A',
     vusd:  '0x5bb9Fa02a3DCCDB4E9099b48e8Ba5841D2e59d51',
-    vnusd: '0x2608A88219BFB34519f635Dd9Ca2Ae971539ca60'
+    vnusd: '0x2608A88219BFB34519f635Dd9Ca2Ae971539ca60',
+    azUSD: '0xf45fde3f484c44cc35bdc2a7fca3ddde0c8f252e'
   },
   methodIds: {
     virtualSwap: '0xa6d67510',
     athSwap:     '0x1bf6318b',
     vnusdSwap:   '0xa6d67510',
+    azUSD:       '0xa6d67510',
     stake:       '0xa694fc3a'
   },
   gasLimit: 1000000,
